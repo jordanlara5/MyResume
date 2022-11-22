22407ebd4c90622d049e1923599ea5196f843ede
Jordan <jordanlara1@hotmail.com>
2022-11-22 09:15:51 -0600

    intial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# Import necessary packages and functions
+from flask import Flask, render_template
+
+# Create our web application
+app = Flask(__name__)
+
+# Create our routes
+@app.route('/')
+def home():
+	return render_template('index.html')
+@app.route('/')
+def details():
+	return render_template('portfolio-details.html')
+# Start our application
+if __name__ == "__main__":
+	app.run(
+		debug=True,
+		# port = 5000
+	)
